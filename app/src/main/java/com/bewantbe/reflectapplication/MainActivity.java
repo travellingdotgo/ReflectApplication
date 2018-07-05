03e5824958bccf9394ffd5507b81a98532375b42
c <c@g.hk>
2018-07-05 14:42:53 +0800

    Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {

--- app/src/main/java/com/bewantbe/reflectapplication/MainActivity.java
+++ app/src/main/java/com/bewantbe/reflectapplication/MainActivity.java
@@ -1,15 +1,28 @@
 package com.bewantbe.reflectapplication;
 
 import android.content.Intent;
+import android.os.Looper;
+import android.os.MessageQueue;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Looper.myQueue().addIdleHandler(new MessageQueue.IdleHandler() {
+            @Override
+            public boolean queueIdle() {
+                Log.i(TAG, "queueIdle");
+
+                return false;
+            }
+        });
     }
 
     @Override
